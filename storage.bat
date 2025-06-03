057707f90c511adb46f1daf9ef33873323acc06f
vitalon2000 <vitalon2000@gmail.com>
2025-06-03 15:48:41 +0800

    Initial commit

---
+++ storage.bat
@@ -0,0 +1,7 @@
+echo "#NaiRuo" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin https://github.com/vitalon2000/NaiRuo.ote
+ add origin https://github.com/vitalon2000/NaiRuo。 gitorigin
